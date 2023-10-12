2ed85eba322a5892bdb1e4922a8bd3ecf89e30b4
Ulf Aslak <ulf@precisdigital.com>
2023-10-12 13:10:47 +0200

    refactor(BaseModel-and-children): Move `_validate_series` to BaseModel parent class
    It was the same in DataFrameModel and SeriesModel, no point in repeating code

--- src/pandabear/model.py
+++ src/pandabear/model.py
@@ -11,22 +11,8 @@ TYPE_DTYPE_MAP = {
     str: np.dtype("O"),
 }
 
-
 @dataclasses.dataclass
 class BaseModel:
-    pass
-
-
-class DataFrameModel(BaseModel):
-    @classmethod
-    def _get_names_and_types(cls):
-        return cls.__annotations__
-
-    @classmethod
-    def _get_fields(cls):
-        names_types = cls._get_names_and_types()
-        return {name: getattr(cls, name) for name in names_types}
-
     @staticmethod
     def _validate_series(se: pd.Series, field: Field, typ: Any):
         dtype = TYPE_DTYPE_MAP.get(typ, typ)
@@ -38,6 +24,16 @@ class DataFrameModel(BaseModel):
             if check_value is not None:
                 if not check_func(se, check_value):
                     raise ValueError(f"Column`{se.name}` did not pass check `{check_name} {check_value}`")
+
+class DataFrameModel(BaseModel):
+    @classmethod
+    def _get_names_and_types(cls):
+        return cls.__annotations__
+
+    @classmethod
+    def _get_fields(cls):
+        names_types = cls._get_names_and_types()
+        return {name: getattr(cls, name) for name in names_types}
 
     @staticmethod
     def _select_series_by_name(df: pd.DataFrame, name: str) -> pd.Series:
@@ -76,18 +72,6 @@ class SeriesModel(BaseModel):
     def _get_field(cls):
         value_name, _ = cls._get_value_name_and_type()
         return getattr(cls, value_name)
-
-    @staticmethod
-    def _validate_series(se: pd.Series, field: Field, typ: Any):
-        dtype = TYPE_DTYPE_MAP.get(typ, typ)
-        if se.dtype != dtype:
-            raise TypeError(f"Expected `{se.name}` dtype {dtype} but found {se.dtype}")
-
-        for check_name, check_func in CHECK_NAME_FUNCTION_MAP.items():
-            check_value = getattr(field, check_name)
-            if check_value is not None:
-                if not check_func(se, check_value):
-                    raise ValueError(f"Column`{se.name}` did not pass check `{check_name} {check_value}`")
 
     @classmethod
     def validate(cls, series: pd.Series):
