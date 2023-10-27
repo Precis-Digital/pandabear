276a4c3e330950a5f28bc9dd94f6bffe0942a820
pierreprecis <pierre@precisdigital.com>
2023-10-27 10:02:51 +0200

    fixed confligt

--- tests/test_model_components.py
+++ tests/test_model_components.py
@@ -1,7 +1,7 @@
-import pytest
 import pandas as pd
+import pytest
 
-from pandabear import model_components, exceptions
+from pandabear import exceptions, model_components
 
 
 def test_is_type_index_wrapped():
@@ -10,13 +10,13 @@ def test_is_type_index_wrapped():
 
 
 def test_is_type_index():
-    assert model_components.is_type_index(model_components.Index[int], 'dummy', 'class_name')
-    assert not model_components.is_type_index(int, 'dummy', 'class_name')
+    assert model_components.is_type_index(model_components.Index[int], "dummy", "class_name")
+    assert not model_components.is_type_index(int, "dummy", "class_name")
     model_components.PANDAS_INDEX_TYPES.append(pd.DatetimeIndex)
-    assert model_components.is_type_index(pd.DatetimeIndex, 'dummy', 'class_name')
-    assert not model_components.is_type_index(str, 'dummy', 'class_name')
+    assert model_components.is_type_index(pd.DatetimeIndex, "dummy", "class_name")
+    assert not model_components.is_type_index(str, "dummy", "class_name")
     with pytest.raises(exceptions.SchemaDefinitionError):
-        model_components.is_type_index(model_components.Index, 'dummy', 'class_name')
+        model_components.is_type_index(model_components.Index, "dummy", "class_name")
 
 
 def test_get_index_type():
